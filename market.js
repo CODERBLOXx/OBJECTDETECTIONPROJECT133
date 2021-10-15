029f3eb1dc9350136fc1dee773591cc886968411
CODERBLOXx <78531418+CODERBLOXx@users.noreply.github.com>
2021-10-15 19:43:27 +0530

    Add files via upload

---
+++ market.js
@@ -0,0 +1,53 @@
+img = "";
+status = "";
+objects = [];
+
+function back_market(){
+    window.location = "index.html";
+}
+
+function preload(){
+    img = loadImage("Market.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500,500);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("market_status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw(){
+    if(status != ""){
+        image(img,0,0,500,500);
+        for(i = 0; i < objects.length; i++){
+            r = 255;
+            g = 255;
+            b = 255;
+            document.getElementById("market_status").innerHTML = "Status: Objects Detected";
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 15,objects[i].y - 15);
+            fill(r,g,b)
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            document.getElementById("market").innerHTML = "There are 4 big objects in the image from which cocossd model has detected " + objects.length + " " + "objects";
+        }
+    } 
+}
+
+function modelLoaded(){
+    status = true;
+    objectDetector.detect(canvas,gotResult);
+    console.log("Object Detector is initialized");
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+    objects = results;
+}
